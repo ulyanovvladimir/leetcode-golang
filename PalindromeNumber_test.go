81b45e6591666f4129b6eeb396997754503b725d
Vladimir Ulianov <dedushka.lenin@gmail.com>
2022-10-12 13:06:49 +0300

    Testify unit tests

--- PalindromeNumber_test.go
+++ PalindromeNumber_test.go
@@ -1,25 +1,15 @@
 package main
 
-import "testing"
-
-type palindromeNumberParameter struct {
-	arg1     int
-	expected bool
-}
-
-var palindromeNumberParameters = []palindromeNumberParameter{
-	palindromeNumberParameter{121, true},
-	palindromeNumberParameter{12, false},
-	palindromeNumberParameter{1, true},
-	palindromeNumberParameter{0, true},
-	palindromeNumberParameter{123321, true},
-	palindromeNumberParameter{12123, false},
-}
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestIsPalindrome(t *testing.T) {
-	for _, test := range palindromeNumberParameters {
-		if output := isPalindrome(test.arg1); output != test.expected {
-			t.Errorf("Output %t not equal to expected %t", output, test.expected)
-		}
-	}
+	assert.True(t, isPalindrome(121))
+	assert.False(t, isPalindrome(12))
+	assert.True(t, isPalindrome(1))
+	assert.True(t, isPalindrome(0))
+	assert.True(t, isPalindrome(123321))
+	assert.False(t, isPalindrome(12123))
 }
